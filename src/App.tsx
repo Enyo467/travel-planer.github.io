fe2b47ca8d8049e718f9dc6b32b2f500ef6521ea
Enyo467 <edyta.kozlowska1@onet.pl>
2026-04-21 19:45:36 +0200

    change: deleting router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainMenu from './MainMenu';
 import PlanDetails from './PlanDetails';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<MainMenu />} />
-                <Route path="/plan/:planName" element={<PlanDetails />} />
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<MainMenu />} />
+            <Route path="/plan/:planName" element={<PlanDetails />} />
+        </Routes>
     )
 }
 
